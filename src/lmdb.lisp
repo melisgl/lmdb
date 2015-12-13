2ecdd3e28a4d373f0bb3df09997f7e1916c7c991
Fernando Borretti <eudoxiahp@gmail.com>
2015-12-13 18:59:59 -0300

    More code

--- src/lmdb.lisp
+++ src/lmdb.lisp
@@ -6,7 +6,18 @@
            :make-environment
            :open-environment
            :environment-statistics
-           :environment-info)
+           :environment-info
+           :free-environment
+           :with-environment
+           :transaction
+           :transaction-environment
+           :transaction-parent
+           :make-transaction
+           :begin-transaction
+           :commit-transaction
+           :database
+           :make-database
+           :open-database)
   (:documentation "The high-level LMDB interface."))
 (in-package :lmdb)
 
@@ -20,7 +31,7 @@
 
 (cffi:use-foreign-library liblmdb)
 
-;;; Interface
+;;; Constants
 
 (defparameter +permissions+ #o664
   "The Unix permissions to use for the database.")
@@ -28,66 +39,105 @@
 ;; Some error codes
 (defparameter +enoent+ 2)
 (defparameter +eacces+ 13)
-(defparameter +eagain+ 3406)
+(defparameter +eagain+ 11)
 
-(defun version-string ()
-  "Return the version string."
-  (format nil "~D.~D.~D"
-          lmdb.low:+version-major+
-          lmdb.low:+version-minor+
-          lmdb.low:+version-patch+))
+;;; Classes
 
 (defclass environment ()
-  ((env :accessor environment-env
-        :initarg :env
-        :documentation "The pointer to the environment handle pointer.")
+  ((handle :reader %handle
+           :initarg :handle
+           :documentation "The pointer to the environment handle pointer.")
    (directory :reader environment-directory
               :initarg :directory
               :documentation "The directory where environment files are stored."))
   (:documentation "Environment handle."))
 
+(defclass transaction ()
+  ((handle :reader %handle
+           :initarg :handle
+           :documentation "The pointer to the transaction environment.")
+   (env :reader transaction-environment
+        :initarg :environment
+        :type environment
+        :documentation "The environment this transaction belongs to.")
+   (parent :reader transaction-parent
+           :initarg :parent
+           :type (or transaction null)
+           :documentation "The parent transaction."))
+  (:documentation "A transaction."))
+
+(defclass database ()
+  ((handle :reader %handle
+           :initarg :handle
+           :documentation "The DBI handle.")
+   (name :reader database-name
+         :initarg :name
+         :type string
+         :documentation "The database name."))
+  (:documentation "A database."))
+
+;;; Constructors
+
 (defun make-environment (directory)
   "Create an environment object."
   (let ((instance (make-instance 'environment
-                                 :env (cffi:foreign-alloc :pointer)
+                                 :handle (cffi:foreign-alloc :pointer)
                                  :directory directory)))
-    (unless (= (lmdb.low:env-create (environment-env instance)) 0)
+    (unless (= (lmdb.low:env-create (%handle instance))
+               0)
       (error "Error creating environment object."))
     instance))
 
-(defun environment-handle (environment)
-  "Return the environment handle."
-  (cffi:mem-ref (environment-env environment) :pointer))
+(defun make-transaction (environment &optional parent)
+  "Create a transaction object."
+  (make-instance 'transaction
+                 :handle (cffi:foreign-alloc :pointer)
+                 :environment environment
+                 :parent parent))
+
+(defun make-database (name)
+  "Create a database object."
+  (make-instance 'database
+                 :handle (cffi:foreign-alloc :pointer)
+                 :name name))
+
+;;; Viscera
+
+(defun handle (object)
+  "Return the handle from an environment, transaction or database."
+  (cffi:mem-ref (%handle object) :pointer))
 
 (defun open-environment (environment)
   "Open the environment connection."
-  (assert (uiop:directory-pathname-p (environment-directory environment)))
-  (let ((return-code (lmdb.low:env-open (environment-handle environment)
-                                        (namestring (environment-directory environment))
-                                        0
-                                        (cffi:make-pointer +permissions+))))
-    (alexandria:switch (return-code)
-      (lmdb.low:+version-mismatch+
-       (error "Version mismatch: the client version is different from the environment version."))
-      (lmdb.low:+invalid+
-       (error "Data corruption: the environment header files are corrupted."))
-      (+enoent+
-       (error "The environment directory doesn't exist."))
-      (+eacces+
-       (error "The user doesn't have permission to use the environment directory."))
-      (+eagain+
-       (error "The environment files are locked by another process."))
-      (0
-       ;; Success
-       )
-      (t
-       (error "Unknown error code."))))
+  (with-slots (directory) environment
+    (assert (uiop:directory-pathname-p directory))
+    (ensure-directories-exist directory)
+    (let ((return-code (lmdb.low:env-open (handle environment)
+                                          (namestring directory)
+                                          0
+                                          (cffi:make-pointer +permissions+))))
+      (alexandria:switch (return-code)
+        (lmdb.low:+version-mismatch+
+         (error "Version mismatch: the client version is different from the environment version."))
+        (lmdb.low:+invalid+
+         (error "Data corruption: the environment header files are corrupted."))
+        (+enoent+
+         (error "The environment directory doesn't exist."))
+        (+eacces+
+         (error "The user doesn't have permission to use the environment directory."))
+        (+eagain+
+         (error "The environment files are locked by another process."))
+        (0
+         ;; Success
+         )
+        (t
+         (error "Unknown error code: ~A" return-code)))))
   environment)
 
 (defun environment-statistics (environment)
   "Return statistics about the environment."
   (cffi:with-foreign-object (stat '(:struct lmdb.low:stat))
-    (lmdb.low:env-stat (environment-handle environment)
+    (lmdb.low:env-stat (handle environment)
                        stat)
     (macrolet ((slot (slot)
                  `(cffi:foreign-slot-value stat
@@ -98,7 +148,7 @@
 (defun environment-info (environment)
   "Return information about the environment."
   (cffi:with-foreign-object (info '(:struct lmdb.low:envinfo))
-    (lmdb.low:env-info (environment-handle environment)
+    (lmdb.low:env-info (handle environment)
                        info)
     (macrolet ((slot (slot)
                  `(cffi:foreign-slot-value info
@@ -106,3 +156,67 @@
                                            ',slot)))
       (list :map-address (cffi:pointer-address (slot lmdb.low:me-mapaddr))
             :map-size (cffi:pointer-address (slot lmdb.low:me-mapsize))))))
+
+(defun begin-transaction (transaction)
+  "Begin the transaction."
+  (with-slots (env parent) transaction
+    (let ((return-code (lmdb.low:txn-begin (handle env)
+                                           (if parent
+                                               (handle parent)
+                                               (cffi:null-pointer))
+                                           0
+                                           (%handle transaction))))
+      (alexandria:switch (return-code)
+        (0
+         ;; Success
+         )
+        ;; TODO rest
+        (t
+         (error "Unknown error code."))))))
+
+(defun commit-transaction (transaction)
+  "Commit the transaction."
+  (lmdb.low:txn-commit (handle transaction)))
+
+(defun abort-transaction (transaction)
+  "Abort the transaction."
+  (lmdb.low:txn-abort (handle transaction)))
+
+(defun open-database (database transaction)
+  "Open a database in a transaction."
+  (let ((return-code (lmdb.low:dbi-open (handle transaction)
+                                        (database-name database)
+                                        0
+                                        (%handle database))))
+    (alexandria:switch (return-code)
+      (0
+       ;; Success
+       t)
+      (t
+       (error "Unknown error code: ~A" return-code))))
+  database)
+
+;;; Destructors
+
+(defun free-environment (environment)
+  "Free the environment."
+  (cffi:foreign-free (%handle environment))
+  (lmdb.low:env-close (handle environment)))
+
+;;; Macros
+
+(defmacro with-environment ((env) &body body)
+  "Free the environment when the body is over."
+  `(progn
+     (unwind-protect
+          (progn ,@body)
+       (free-environment ,env))))
+
+;;; Utilities
+
+(defun version-string ()
+  "Return the version string."
+  (format nil "~D.~D.~D"
+          lmdb.low:+version-major+
+          lmdb.low:+version-minor+
+          lmdb.low:+version-patch+))

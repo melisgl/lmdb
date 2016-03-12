c417866fbeed9c2486aace8c41ab53c761c36b8a
Fernando Borretti <eudoxiahp@gmail.com>
2016-03-12 10:58:51 -0300

    Add docs, update .gitignore

---
+++ docs/manifest.lisp
@@ -0,0 +1,7 @@
+(:docstring-markup-format :scriba
+ :systems (:lmdb)
+ :documents ((:title "LMDB"
+              :authors ("Fernando Borretti")
+              :output-format (:type :multi-html
+                              :template :minima)
+              :sources ("manual.scr"))))
